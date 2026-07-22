8d2182c28f969cbb5768d16848013212f4860905
samheughan2224467-ship-it <samheughan2224467@gmail.com>
2026-07-22 12:27:50 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,23 @@
-async function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-  if (!email || !password) {
-    alert("Please enter your email and password.");
-    return;
-  }
+const firebaseConfig = {
+  apiKey: "AIzaSyCBTOGG0DZv7eIa0U-XZFCcXOYAf8IPT-0",
+  authDomain: "sentrix-vxe.firebaseapp.com",
+  projectId: "sentrix-vxe",
+  storageBucket: "sentrix-vxe.firebasestorage.app",
+  messagingSenderId: "392250155609",
+  appId: "1:392250155609:web:136617e31f25bfef6bc748",
+  measurementId: "G-23JWKYB2YV"
+};
 
-  try {
-    await window.signInWithEmailAndPassword(
-      window.auth,
-      email,
-      password
-    );
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-    alert("✅ Login successful!");
-
-    window.location.href = "dashboard.html";
-
-  } catch (error) {
-    alert(error.message);
-  }
-}
+window.auth = auth;
+window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
+window.signInWithEmailAndPassword = signInWithEmailAndPassword;
